2c068d5e6169e8867846b9f36908d65c6e152f2b
tomek1798 <tomek1798@o2.pl>
2017-04-19 13:44:23 +0200

    Add files via upload

---
+++ zadanie1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Xampp </title>
+	</head>
+	<body>
+		<form action="textarea.php" method="get">
+		Imie: <input type="text" name="imie"><br>
+		Nazwisko: <input type="text" name="nazwisko"<br><br>
+		<input type="submit">
+		</form>
+	</body>
+</html>
---
+++ zadanie1/textarea.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title> Xampp </title>
+	</head>
+	<body>
+	Twoje imie: <?php echo $_GET["imie"]; ?><br>
+	Twoje nazwisko: <?php echo $_GET["nazwisko"]; ?>
+	</body>
+</html>
