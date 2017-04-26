f2523e04709aff9294082c3505445baf8f86b2cb
tomek1798 <tomek1798@o2.pl>
2017-04-26 14:01:03 +0200

    Add files via upload

---
+++ sortowanieliczb/index.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title> Xampp </title>
+	</head>
+	<body>
+		<form action="sortowanie.php" method="get">
+		Podaj pierwszą liczbę: <input type="text" name="a"><br>
+		Podaj drugą liczbę: <input type="text" name="b"<br><br>
+		Podaj trzecią liczbę: <input type="text" name="c"<br><br>
+		Podaj czwartą liczbę: <input type="text" name="d"<br><br>
+		Podaj piątą liczbę: <input type="text" name="e"<br><br>
+		<input type="submit">
+		</form>
+	</body>
+</html>
---
+++ sortowanieliczb/sortowanie.php
@@ -0,0 +1,16 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<title>Sortowanie</title>
+	</head>
+	<body>
+		<?php
+			$liczby = array($_GET["a"],$_GET["b"],$_GET["c"],$_GET["d"],$_GET["e"]);
+			sort($liczby);
+			echo"</br>Posortowane liczby:</br>";
+			for ($i =0; $i < count($liczby); $i++)
+			echo "Liczba ".($i+1).": ".$liczby[$i]."</br>";
+		?>
+	</body>
+</html>
+			
